32c57af18bf693b237ad8f501c200ad0b5bdebd8
swiftdev69 <swiftdev69@gmail.com>
2021-07-29 10:27:43 +0530

    update

--- packages/health/android/src/main/kotlin/cachet/plugins/health/HealthPlugin.kt
+++ packages/health/android/src/main/kotlin/cachet/plugins/health/HealthPlugin.kt
@@ -118,7 +118,8 @@ class HealthPlugin(val activity: Activity, val channel: MethodChannel) : MethodC
             BODY_FAT_PERCENTAGE -> DataType.TYPE_BODY_FAT_PERCENTAGE
             HEIGHT -> DataType.TYPE_HEIGHT
             WEIGHT -> DataType.TYPE_WEIGHT
-            STEPS -> DataType.TYPE_STEP_COUNT_DELTA
+           // STEPS -> DataType.TYPE_STEP_COUNT_DELTA
+            STEPS -> DataType.AGGREGATE_STEP_COUNT_DELTA
             ACTIVE_ENERGY_BURNED -> DataType.TYPE_CALORIES_EXPENDED
             HEART_RATE -> DataType.TYPE_HEART_RATE_BPM
             BODY_TEMPERATURE -> HealthDataTypes.TYPE_BODY_TEMPERATURE
