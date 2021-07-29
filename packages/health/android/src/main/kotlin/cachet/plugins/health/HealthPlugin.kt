131cea22b37eb38169a01ae612b1f8319cffd16d
swiftdev69 <swiftdev69@gmail.com>
2021-07-29 21:48:40 +0530

    fix change

--- packages/health/android/src/main/kotlin/cachet/plugins/health/HealthPlugin.kt
+++ packages/health/android/src/main/kotlin/cachet/plugins/health/HealthPlugin.kt
@@ -199,7 +199,7 @@ class HealthPlugin(val activity: Activity, val channel: MethodChannel) : MethodC
                 val newRequest =  DataReadRequest.Builder()
                         .aggregate(DataType.TYPE_CALORIES_EXPENDED, DataType.AGGREGATE_CALORIES_EXPENDED)
                         .aggregate(ESTIMATED_STEP_DELTAS, DataType.AGGREGATE_STEP_COUNT_DELTA)
-                        .bucketByTime(1, TimeUnit.DAYS)
+                        .bucketByActivitySegment(1, TimeUnit.DAYS)
                         .setTimeRange(startTimeFromFlutter, endTimeFromFlutter, TimeUnit.SECONDS)
 
                         /*.bucketByActivitySegment(1, TimeUnit.MILLISECONDS)
